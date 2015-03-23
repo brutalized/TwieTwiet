f0d1a5ac811452db5ecfd1cc6f8186517a18ee24
Lesley <unit.ix@hotmail.com>
2015-03-23 09:01:33 +0100

    updated Gui

--- classes/gui.py
+++ classes/gui.py
@@ -5,7 +5,17 @@ from images_rc import *
 
 
 # Load the UI
-form_class = uic.loadUiType("TwieTwiet.ui")[0]
+form_class = uic.loadUiType("classes/TwieTwiet.ui")[0]
+
+class ClickableLabel(QtGui.QLabel):
+	def __init__(self, clickFunction=None, *args):
+		super().__init__(*args)
+		self.clickFunction = clickFunction
+
+	def mouseReleaseEvent(self, event):
+		if self.clickFunction:
+			self.clickFunction()
+
 
 class Gui(QtGui.QMainWindow, form_class):
 
@@ -19,42 +29,44 @@ class Gui(QtGui.QMainWindow, form_class):
 		self.header.setResizeMode(QtGui.QHeaderView.Stretch)
 		self.header.setBackgroundRole(QtGui.QPalette.NoRole)
 		
-		self.url = 'http://th00.deviantart.net/fs71/PRE/f/2011/285/c/2/lol_face_by_rober_raik-d4cm1yl.png'    
-		data = urllib.request.urlopen(self.url).read()
-		pixmap = QtGui.QPixmap()
-		pixmap.loadFromData(data)
+		#user image
+		self.url = "http://eaassets-a.akamaihd.net/battlelog/prod/emblem/219/323/256/2955058837069862971.png"
+		pixmap = self.getUserImage(self.url)
 
-		self.url2 = 'http://th00.deviantart.net/fs71/PRE/f/2011/285/c/2/lol_face_by_rober_raik-d4cm1yl.png'    
-		data = urllib.request.urlopen(self.url2).read()
-		pixmap2 = QtGui.QPixmap()
-		pixmap2.loadFromData(data)
+		#user image2
+		self.url2 = "http://eaassets-a.akamaihd.net/battlelog/prod/emblem/219/323/256/2955058837069862971.png"    
+		pixmap2 = self.getUserImage(self.url2)
 
 		#tweet1
 		self.profile_picture = QtGui.QTableWidgetItem()
 		self.profile_picture.setBackground(QtGui.QBrush(pixmap))
-		self.tweet = QtGui.QTableWidgetItem("lol")
-		self.name = QtGui.QTableWidgetItem("lol")
-		self.time = QtGui.QTableWidgetItem("lol")
+		self.tweet = ClickableLabel(self.showDetails, self.styleTweet(" "))
+		self.tweet.setTextFormat(QtCore.Qt.RichText)
+		self.tweet.setFont(QtGui.QFont("arial", 14))
+		self.tweet.setAlignment(QtCore.Qt.AlignCenter)
+		self.name = QtGui.QTableWidgetItem(" ")
+		self.name.setTextColor(QtGui.QColor(51,204,255))
+		self.name.setTextAlignment(QtCore.Qt.AlignCenter)
+		self.time = QtGui.QTableWidgetItem(" ")
+		self.time.setTextAlignment(QtCore.Qt.AlignCenter)
 
 		#tweet2
 		self.profile_picture2 = QtGui.QTableWidgetItem()
 		self.profile_picture2.setBackground(QtGui.QBrush(pixmap2))
-		self.tweet2 = QtGui.QTableWidgetItem("lol")
-		self.name2 = QtGui.QTableWidgetItem("lol")
-		self.time2 = QtGui.QTableWidgetItem("lol")
+		self.tweet2 = ClickableLabel(self.showDetails, " ")
+		self.tweet2.setTextFormat(QtCore.Qt.RichText)
+		self.tweet2.setFont(QtGui.QFont("arial", 14))
+		self.tweet2.setAlignment(QtCore.Qt.AlignCenter)
+		self.name2 = QtGui.QTableWidgetItem(" ")
+		self.name2.setTextColor(QtGui.QColor(51,204,255))
+		self.name2.setTextAlignment(QtCore.Qt.AlignCenter)
+		self.time2 = QtGui.QTableWidgetItem(" ")
+		self.time2.setTextAlignment(QtCore.Qt.AlignCenter)
 
+		self.tableWidget.setCellWidget(0,0,self.tweet)
+		self.tableWidget.setCellWidget(1,0,self.tweet2)
 
-		self.tweet.setFlags(QtCore.Qt.ItemIsSelectable | QtCore.Qt.ItemIsEnabled)
-		self.tweet2.setFlags(QtCore.Qt.ItemIsSelectable | QtCore.Qt.ItemIsEnabled)
-		self.tweet.setFlags(QtCore.Qt.NoItemFlags)
-		self.tweet2.setFlags(QtCore.Qt.NoItemFlags)
-		self.tweet.setTextAlignment(QtCore.Qt.AlignCenter)
-		self.tweet2.setTextAlignment(QtCore.Qt.AlignCenter)
-		self.tableWidget.setItem(0,0,self.tweet)
-		self.tableWidget.setItem(1,0,self.tweet2)
-
-		self.pushButton.clicked.connect(self.update)
-		self.tableWidget.cellClicked.connect(self.showDetails)
+		self.pushButton.clicked.connect(self.updateUI)
 		self.clickable = True
 
 	def showDetails(self):
@@ -88,19 +100,41 @@ class Gui(QtGui.QMainWindow, form_class):
 			self.time2.setFlags(QtCore.Qt.NoItemFlags)
 			self.clickable = False
 
-	def update(self, tweet, tweet2):
+	def getUserImage(self, url): 
+		data = urllib.request.urlopen(url).read()
+		pixmap = QtGui.QPixmap()
+		pixmap.loadFromData(data)
+		return pixmap
+
+	def updateUI(self, tweet, tweet2):
 		""" Update tweet data """ 
-		self.profile_picture.setText(tweet.userImage)
-		self.profile_picture2.setText(tweet2.userImage)
+		pixmap = getUserImage(tweet.userImage)
+		self.profile_picture.setBackground(QtGui.QBrush(pixmap))
+		pixmap2 = getUserImage(tweet2.userImage)
+		self.profile_picture2.setBackground(QtGui.QBrush(pixmap2))
 
 		self.name.setText(tweet.userName)
 		self.name2.setText(tweet2.userName)
 
-		self.tweet.setText(tweet.message)
-		self.tweet2.setText(tweet2.message)
+		self.tweet.setText(self.styleTweet(tweet.message))
+		self.tweet2.setText(self.styleTweet(tweet2.message))
 
 		self.time.setText(tweet.date)
 		self.time2.setText(tweet2.date)
+
+	def styleTweet(self, message):
+		words = message.split()	
+		for i in range(len(words)):
+			if words[i][0] == "#":
+				words[i] = '<b style="font-size: 14px;">{}</b>'.format(words[i])
+			elif words[i][0] == "@":
+				words[i] = '<span style="color:#33ccff;">{}</span>'.format(words[i])
+			elif words[i][0:4] == "http":
+				words[i] = '<span style="color:#89dee3; text-decoration: underline;">{}</span>'.format(words[i])
+		styledString = " ".join(words)
+
+
+		return styledString
 
 
 if __name__ == "__main__":
