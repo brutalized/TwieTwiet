377e29595f3bfd1722e3324ebfc4728e0d7f8fc2
Tendacen <mtnderijk@hotmail.com>
2015-03-23 17:36:49 +0100

    Added GUI connection to TwieTwiet.py

--- TwieTwiet.py
+++ TwieTwiet.py
@@ -3,39 +3,38 @@
 ''' TwieTwie Application by Arend-Eric, Lesley and Micha '''
 
 import re
+import sys
+from PyQt4 import QtGui
 from classes.tweets import Tweets
 from classes.rhyme import Entry, Rhyme
+from classes.gui import Gui
 
 def main():
-    db = Tweets() # init db
-    tweets = db.getTweets() # get all tweets
-    rhyme = Rhyme()
-
-
-    usedTweet = tweets[0].message.split()
-    wantedRhymeWord = re.sub(r'\W+', '', usedTweet[len(usedTweet)-1])
-    
-    for tweet in tweets:
-        parts = tweet.message.split()
-        lw = re.sub(r'\W+', '', parts[len(parts)-1])
-
-        # Filter if the last word is a URL or empty
-        if(lw.find('http') != -1 and lw.strip() != ''):
-            continue
-
-        # Ignore if same word as wanted word
-        if(wantedRhymeWord == lw):
-            continue
-
-        try:
-            if(rhyme.compare(wantedRhymeWord, lw)):
-                print(tweets[0].message, tweet.message, sep='\n')
-                print()
-                print()
-        except:
-            continue            
-
-        
+	db = Tweets() # init db
+	tweets = db.getTweets() # get all tweets
+	rhyme = Rhyme()
+	
+	twietwiets = []
+	usedTweet = tweets[0].message.split()
+	wantedRhymeWord = re.sub(r'\W+', '', usedTweet[len(usedTweet)-1])
+	
+	for tweet in tweets:
+		parts = tweet.message.split()
+		lw = re.sub(r'\W+', '', parts[len(parts)-1])
+		
+		try:
+			if rhyme.compare(wantedRhymeWord, lw):
+				twietwiets.append((tweets[0], tweet))
+		except:
+			continue
+	
+	print('Enjoy the GUI!')
+	
+	app = QtGui.QApplication(sys.argv)
+	gui = Gui(None)
+	gui.updateUI(twietwiets[0][0], twietwiets[0][1])
+	gui.show()
+	app.exec_()
 
 if __name__ == "__main__":
-    main()
+	main()
